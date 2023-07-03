2d3655dd55b33f8662ba7b69be5dc94e8bf03b8e
rabiahaucha <rabiahaucha9@gmail.com>
2023-07-03 16:41:58 +0200

    donewithtoday

--- src/store/index.js
+++ src/store/index.js
@@ -1,19 +1,100 @@
 import { createStore } from 'vuex'
 
+const dataURL = "https://rabiahaucha.github.io/vue-portfolio-JSON.SERVE/index.json";
+
 export default createStore({
   state: {
-    skills :null,
-    testimomials: null,
-    projects:null,
-    spinner:null,
+    Skills :null,
+    Testimonials: null,
+    Projects:null,
+    Education:null,
+    Spinner:false,
   },
   getters: {
     
   },
   mutations: {
+    setSkills(state,data){
+      state.Skills = data;
+    },
+    setTestimonials(state,data){
+      state.Testimonials= data;
+    },
+    setEducation(state,data){
+      state.Education = data;
+    },
+    setProjects(state,data){
+      state.Projects = data;
+    },
+    setSpinner(state,value){
+      state.value = value;
+    },
+
   },
   actions: {
+    async fetchSkills(context){
+      try{
+        let res = await fetch (dataURL);
+        let {Skills} = await res.json();
+
+        if(Skills){
+          context.commit("setSkills",Skills);
+          context.commit("setSpinner",false);
+        }else{
+          context.commit("setSpinner",true);
+        } 
+       } catch(e){
+          console.log(e.message);
+        }    
+    },
+    async fetchTestimonials(context){
+      try{
+        let res = await fetch (dataURL);
+        let {Testimonials} = await res.json();
+
+        if(Testimonials){
+          context.commit("setTestimonials",Testimonials);
+          context.commit("setSpinner",false);
+        }else{
+          context.commit("setSpinner",true);
+        } 
+       } catch(e){
+          console.log(e.message);
+        }    
+    },
+    async fetchProjects(context){
+      try{
+        let res = await fetch (dataURL);
+        let {Projects} = await res.json();
+
+        if(Projects){
+          context.commit("setProjects",Projects);
+          context.commit("setSpinner",false);
+        }else{
+          context.commit("setSpinner",true);
+        } 
+       } catch(e){
+          console.log(e.message);
+        }    
+    },
+    async fetchEducation(context){
+      try{
+        let res = await fetch (dataURL);
+        let {Education} = await res.json();
+
+        if(Education){
+          context.commit("setEducation",Education);
+          context.commit("setSpinner",false);
+        }else{
+          context.commit("setSpinner",true);
+        } 
+       } catch(e){
+          console.log(e.message);
+        }    
+       
+    },
+    
   },
   modules: {
-  }
-})
+  },
+});
--- src/views/SkillsView.vue
+++ src/views/SkillsView.vue
@@ -1,33 +1,66 @@
 <template>
-    <div class="mt-5 container">
-        <div class="row">
-            <div class="col-12 col-md-6">
-                <i class="bi bi-bootstrap-fill text-black icon"  style="color: black;"></i> 
-               <h6>It went through such rapid contortions that the little bear was forced to change his hold on it so many times he became confused in the darkness, and could not, for the life of him, tell whether he held the sheep right side up, or upside down. But</h6>
-       
-               <i class="bi bi-filetype-css icon"  style="color: black"></i><h6>It went through such rapid contortions that the little bear was forced to change his hold on it so many times he became confused in the darkness, and could not, for the life of him, tell whether he held the sheep right side up, or upside down. But</h6>
-            </div>
-
-            <div class="col-12 col-md-6">
-            
-                <i class="bi bi-filetype-js icon"  style="color: black"></i><h6>It went through such rapid contortions that the little bear was forced to change his hold on it so many times he became confused in the darkness, and could not, for the life of him, tell whether he held the sheep right side up, or upside down. But</h6>
-        
-                <i class="bi bi-filetype-html icon"  style="color: black"></i> <h6>It went through such rapid contortions that the little bear was forced to change his hold on it so many times he became confused in the darkness, and could not, for the life of him, tell whether he held the sheep right side up, or upside down. But</h6>
-            </div>
+    <div>
+        <canvas id="pie-chart"> </canvas>
    
+    <div class="my-5 container">
+        <div class="row">
+            <h1>MY <span style="color:#a37474;">SKILLS</span></h1>
+            <div class="col-12 col-md-4" v-for="data in Skills" :key="data.id">
+                <img :src="data.image" alt="" style="width:9rem;height:9rem;">
+                <p>{{ data.description }}</p>
+             </div>   
         </div>
     </div>
+    </div>   
 </template>
 
 <script>
     export default {
-        
+        computed:{
+            Skills(){
+                return this.$store.state.Skills
+            }
+        },
+        mounted(){
+            this.$store.dispatch('fetchSkills')
+        }
     }
+    
 </script>
 
 <style scoped>
+@import url("https://fonts.googleapis.com/css?family=Open+Sans&display=swap");
 
-.icon{
-    font-size: 5rem;
+* {
+  box-sizing: border-box;
 }
+
+body {
+  background-color: #f9fafb;
+  font-family: "Open Sans", sans-serif;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  min-height: 100vh;
+  margin: 0;
+}
+.container {
+  background-color: #fff;
+  border-radius: 10px;
+  box-shadow: 0 2px 10px rgba(0, 0, 0, 0.3);
+  
+}
+@media all and (max-width: 780px) {
+  .container {
+    width: auto;
+  }
+};
+
+@media all and (max-width: 400px) {
+  .container {
+    padding: 0px 20px;
+  }
+}
+
+
 </style>
